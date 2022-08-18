5248e987b0d127e269ffdb67f80dd62980aa91cd
asaal-dicorholding <andreas.saal@gmx.net>
2022-08-18 13:39:22 +0200

    add- my retros script

---
+++ my-retros-page.js
@@ -0,0 +1,52 @@
+"use strict";
+
+let userId;
+
+MemberStack.onReady.then(function(member) {   
+	if (member.loggedIn) {
+        userId = member["id"];
+	}
+})
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+	getUserData();
+}
+
+function getUserData() {
+	const token = MemberStack.getToken();
+  
+	if (userId && token) {
+        fetch(`https://hnva3v8a12.execute-api.eu-west-2.amazonaws.com/test/user/${userId}`, {
+            method: 'GET',
+            headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+            },
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            }
+            throw new Error(response.message);
+            })
+        .then((data) => {
+            removeNonFavorites(data.purchases);
+        })
+        .catch((error) => {
+            console.error(error.message);
+        });
+        }
+}
+
+function removeNonFavorites(purchases) {
+    purchases.forEach(purchase => {
+        const tag = document.getElementById(purchase);
+        const divToDelete = tag.closest('div[role="listitem"]');
+
+        if (divToDelete) {
+            divToDelete.remove()
+        }
+    });
+}
--- retro-page.js
+++ retro-page.js
@@ -23,62 +23,66 @@ function init() {
 
 function buyRetro() {
 	const data = { "slug": productSlug };  
-  const token = MemberStack.getToken();
+    const token = MemberStack.getToken();
   
-  if (userId && token) {
-  fetch(`https://hnva3v8a12.execute-api.eu-west-2.amazonaws.com/test/user/${userId}`, {
-  method: 'PUT', 
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`,
-  },
-  body: JSON.stringify(data),
-})
-.then((response) => response.json())
-.then((data) => {
-  console.log('Success:', data);
-  if (data) {
-  // TODO: add spinner while buying process
-  // TODO: resolve without reload
-  	window.location.reload();
-  }
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
-  }
+    if (userId && token) {
+        fetch(`https://hnva3v8a12.execute-api.eu-west-2.amazonaws.com/test/user/${userId}`, {
+        method: 'PUT', 
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify(data),
+        }).then((response) => {
+            if (response.ok) {
+                return response.json()
+            } 
+            throw new Error(response.message);
+        }).then((data) => {
+            console.log('Success:', data);
+            if (data) {
+                // TODO: add spinner while buying process
+                // TODO: resolve without reload
+                window.location.reload();
+            }
+        }).catch((error) => {
+            console.error(error.message);
+        });
+    }
 }
 
 function getUserData() {
 	const token = MemberStack.getToken();
   
 	if (userId && token) {
-    fetch(`https://hnva3v8a12.execute-api.eu-west-2.amazonaws.com/test/retro/user/${userId}?slug=${productSlug}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`,
-    },
-  })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log('Success:', data);
-    // user has already bought this retro
-    if (data.url) {
-    	const downloadButton = document.getElementById('download-retro');
-        downloadButton.classList.remove('hidden');
-        downloadButton.href = data.url;      
-    }
-    else {
-    	const buyButton = document.getElementById('buy-retro');
-        buyButton.classList.remove('hidden')
-        buyButton.addEventListener('click', buyRetro);
-    }
-    setCounter(data.totalPurchases, data.purchasesThisMonth);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+        fetch(`https://hnva3v8a12.execute-api.eu-west-2.amazonaws.com/test/retro/user/${userId}?slug=${productSlug}`, {
+        method: 'GET',
+        headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+        },
+        }).then((response) => {
+            if (response.ok) {
+                return response.json()
+            }
+            throw new Error(response.message);
+        }).then((data) => {
+            console.log('Success:', data);
+            // user has already bought this retro
+            if (data.url) {
+                const downloadButton = document.getElementById('download-retro');
+                downloadButton.classList.remove('hidden');
+                downloadButton.href = data.url;      
+            }
+            else {
+                const buyButton = document.getElementById('buy-retro');
+                buyButton.classList.remove('hidden')
+                buyButton.addEventListener('click', buyRetro);
+            }
+            setCounter(data.totalPurchases, data.purchasesThisMonth);
+        }).catch((error) => {
+        console.error(error.message);
+    });
 }
 }
 
