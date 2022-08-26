d06f3949407f4326cc9ffbc3923e1d58d0453a11
asaal-dicorholding <andreas.saal@gmx.net>
2022-08-26 09:44:44 +0200

    add- favorite page logic

---
+++ favorites-page.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let userId;
+
+MemberStack.onReady.then(async(member) => {   
+	if (member.loggedIn) {
+        const metadata = await member.getMetaData(); 
+        const favorites = metadata.favorites || []; 
+        removeNonFavoriteRetros(favorites);
+	}
+})
+
+function removeNonFavoriteRetros(favorites) {
+    const allRetroElements = document.querySelectorAll('p[role="id_finder"]');
+    const allRetroSlugs = [];
+
+    allRetroElements.forEach(el => {
+        allRetroSlugs.push(el.id);
+    });
+
+    allRetroSlugs.forEach(slug => {
+        // if slug does not exist in favorites, remove the whole element
+        if (favorites.indexOf(slug) === -1) {
+            const tag = document.getElementById(slug);
+            const divToDelete = tag.closest('div[role="listitem"]');
+
+            if (divToDelete) {
+                divToDelete.remove();
+            }
+        }        
+    });
+}
