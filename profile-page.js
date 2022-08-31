2344617464f01aec6f6f89f7358e3b647b7b09f1
asaal-dicorholding <andreas.saal@gmx.net>
2022-08-31 14:59:06 +0200

    add- remove required fields for freemium members

--- profile-page.js
+++ profile-page.js
@@ -8,12 +8,26 @@ function init() {
     MemberStack.onReady.then(function(member) {   
         if (member.loggedIn) {
             const userPlan = member.membership["id"];
-            
-            if (userPlan === PREMIUM_PLAN) checkProfileData(member);
+
+            if (userPlan === PREMIUM_PLAN) {
+                checkProfileData(member);
+            } 
+            else {
+                removeRequiredAttributes();
+            }
         }
     })    
 }
 
 function checkProfileData(member) {
     if (!member.address || !member.city || !member.company || !member.country || !member.name || !member.vat || !member.zipcode) document.getElementById('profile-warning-message').classList.remove('hidden');
+}
+
+function removeRequiredAttributes() {
+    document.getElementById('company-name-input').required = false;
+    document.getElementById('vat-input').required = false;
+    document.getElementById('address-input').required = false;
+    document.getElementById('zip-code-input').required = false;
+    document.getElementById('city-input').required = false;
+    document.getElementById('country-input').required = false;
 }
